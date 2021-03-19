0d8857aad9508358e6609c34b4518fa735af98bc
@BlueTechno <malavervj@gmail.com>
2021-03-19 18:38:27 -0400

    Let's try that

--- scripts/chopper.js
+++ scripts/chopper.js
@@ -4,14 +4,16 @@ chopper.constructor = () => {
 	return unit;
 }
 
-if (!unit.IsFlying()) weapons.remove("bomber"); return;
-
+if (!unit.IsFlying()) {
+	weapons.remove("bomber");
+	return;
+}
 else (unit.IsFlying()) {
-	weapons.add(new Weapon("bomber")){{
-		reload = 25f;
+	weapons.add(new Weapon("bomber")) {{
+		reload = 25;
 		ejectEffect = Fx.none;
 		ignoreRotation = true;
 		shootSound = Sounds.none;
-		inaccuarcy = 12f
+		inaccuarcy = 12;
 	}};
 }
