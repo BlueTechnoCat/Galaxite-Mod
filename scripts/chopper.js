e8ae5eda6882354237093d5790c49af5c63e65d1
@BlueTechno <malavervj@gmail.com>
2021-03-19 17:18:22 -0400

    Visual Stidio is back!

---
+++ scripts/chopper.js
@@ -0,0 +1,17 @@
+const chopper = extendContent(UnitType, "chopper", {});
+chopper.constructor = () => {
+	const unit = extend(UnitEntity, {})
+	return unit;
+}
+
+if (!unit.IsFlying()) weapons.remove("bomber"); return;
+
+else (unit.IsFlying()) {
+	weapons.add(new Weapon("bomber")){{
+		reload = 25f;
+		ejectEffect = Fx.none;
+		ignoreRotation = true;
+		shootSound = Sounds.none;
+		inaccuarcy = 12f
+	}};
+}
