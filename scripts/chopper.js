3edb2a2ab0636c167177fd2c8afa61e2dea4e970
@BlueTechno <malavervj@gmail.com>
2021-04-10 20:33:29 -0400

    Added the cannon and shooter descriptions (and made the cannon a flying unit)

--- scripts/chopper.js
+++ scripts/chopper.js
@@ -4,15 +4,16 @@ chopper.constructor = () => {
 	return unit;
 }
 
-if (!unit.IsFlying()) {
+if (!unit.flying()) {
 	weapons.remove("bomber");
 }
-else if(unit.IsFlying()) {
+else if(unit.flying()) {
 	weapons.add(new Weapon("bomber")); {{
 			reload = 25;
 			ejectEffect = Fx.none;
 			ignoreRotation = true;
 			shootSound = Sounds.none;
+			damage = 130f;
 			inaccuarcy = 12;
 		}};
 }
